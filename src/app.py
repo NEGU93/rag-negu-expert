b16e820328f4691ccdbfe84793521eb1dd289b7a
J Agustin Barrachina <joseagustin.barra@gmail.com>
2025-11-03 14:22:41 +0100

    Fix bug in chat

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ def get_conversation_chain():
 
 
 def chat(question, history):
+    conversation_chain = get_conversation_chain()
     result = conversation_chain.invoke({"question": question})
     return result["answer"]
 
